736987f98cf9a24a9384b8556e5206e6e881d26a
Felippe Scheidt <felippescheidt@gmail.com>
2026-05-07 20:34:02 -0300

    funcoes js

---
+++ javascript/01/loop.js
@@ -0,0 +1,11 @@
+// declara a função 
+function imprimeTitulo(){
+  document.write("<h1>titulo 1</h1>");
+  document.write("<h2>titulo 2</h2>");
+  for(let i=0; i < 10; i++){
+    document.write(`<h3>titulo ${i+1}</h3>`);
+  }
+}
+// chama a função 
+imprimeTitulo()
+
--- javascript/loop.js
+++
@@ -1,6 +0,0 @@
-document.write("<h1>titulo 1</h1>");
-document.write("<h2>titulo 2</h2>");
-
-for(let i=0; i < 10; i++){
-  document.write(`<h3>titulo ${i+1}</h3>`);
-}
